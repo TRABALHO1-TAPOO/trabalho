209e384d02f1ef5fed9543b705810e87aebdc970
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2025-05-01 20:44:48 -0300

    feat: home buttons and logo on config

--- diario_saude/diario_saude/ViewModels/HomePageViewModel.cs
+++ diario_saude/diario_saude/ViewModels/HomePageViewModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// HomePageViewModel.cs
+using diario_saude.ViewModels;
+using ReactiveUI;
+using System.Reactive;
 
 namespace diario_saude.ViewModels
 {
-    class HomePageViewModel : ViewModelBase
+    public class HomePageViewModel : ViewModelBase
     {
+        private readonly MainWindowViewModel _mainWindowViewModel;
+
+        public ReactiveCommand<Unit, Unit> NavigateToDailyRecordCommand { get; }
+        public ReactiveCommand<Unit, Unit> NavigateToRecordHistoryCommand { get; }
+
+        public HomePageViewModel(MainWindowViewModel mainWindowViewModel)
+        {
+            _mainWindowViewModel = mainWindowViewModel;
+
+            NavigateToDailyRecordCommand = ReactiveCommand.Create(() =>
+            {
+                _mainWindowViewModel.NavigateTo<DailyRecordPageViewModel>();
+            });
+
+            NavigateToRecordHistoryCommand = ReactiveCommand.Create(() =>
+            {
+                _mainWindowViewModel.NavigateTo<RecordHistoryPageViewModel>();
+            });
+        }
     }
 }
