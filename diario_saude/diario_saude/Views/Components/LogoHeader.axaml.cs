64271b6a53130199219469405ae7b2fbcf282bd7
Pedro Inácio Rodrigues Pontes <pedroinaciopontes@gmail.com>
2025-05-01 21:12:56 -0300

    refactor: logo header

---
+++ diario_saude/diario_saude/Views/Components/LogoHeader.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace diario_saude.Views.Components
+{
+    public partial class LogoHeader : UserControl
+    {
+        public LogoHeader()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
